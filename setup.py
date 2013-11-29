cb00c0dffaaecc53eb8b0b88a782391e12e344cc
Mikel Larreategi <mlarreategi@codesyntax.com>
2013-11-29 13:24:20 +0100

    docs

--- setup.py
+++ setup.py
@@ -16,9 +16,9 @@ setup(name='bise.biodiversitystrategy',
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
+      author='Mikel Larreategi',
+      author_email='mlarreategi@codesyntax.com',
+      url='https://github.com/eea/bise.biodiversitystrategy',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['bise'],
@@ -40,6 +40,6 @@ setup(name='bise.biodiversitystrategy',
       # addcontent support from paster and before you distribute
       # your package.
       setup_requires=["PasteScript"],
-      paster_plugins = ["ZopeSkel"],
+      paster_plugins=["ZopeSkel"],
 
       )
